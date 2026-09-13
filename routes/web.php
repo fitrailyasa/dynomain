f78452116e52491ce6fbaa630ad102c9f52721fa
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-09-14 06:18:18 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 })->name('home');
 
 Route::get('/dashboard', [AdminDashboardController::class, 'index'])
